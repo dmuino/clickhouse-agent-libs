60a86abe7a4b0cea4d570c1d2008d58d2508e673
Daniel Muino <dmuino@netflix.com>
2023-11-18 17:48:51 -0800

    Create the serviceName dir under /var/lib
    Needed by clickhouse-keeper for some reason

--- dirs.go
+++ dirs.go
@@ -8,6 +8,17 @@ import (
 )
 
 func SetupDirs(serviceName string) {
+	// Get the user clickhouse which will own the directories
+	clickhouseUser, err := user.Lookup("clickhouse")
+	if err != nil {
+		logger.Fatalf("Error looking up clickhouse user: %v", err)
+	}
+
+	clickhouseUid, err := strconv.Atoi(clickhouseUser.Uid)
+	logger.CheckErr(err)
+	clickhouseGid, err := strconv.Atoi(clickhouseUser.Gid)
+	logger.CheckErr(err)
+
 	// Remove the directory /var/log/$serviceName
 	origLogDir := fmt.Sprintf("/var/log/%s", serviceName)
 	if err := os.RemoveAll(origLogDir); err != nil {
@@ -17,18 +28,30 @@ func SetupDirs(serviceName string) {
 		logger.Fatalf("Error removing /var/lib/clickhouse: %v", err)
 	}
 
-	finalLogDir := fmt.Sprintf("/logs/%s", serviceName)
-	err := os.MkdirAll(finalLogDir, os.ModePerm)
+	varLibDir := fmt.Sprintf("/var/lib/%s", serviceName)
+	if err := os.RemoveAll(varLibDir); err != nil {
+		logger.Fatalf("Error removing %s: %v", varLibDir, err)
+	}
+	dataDir := fmt.Sprintf("/data/%s", serviceName)
+	if err := os.MkdirAll(dataDir, os.ModePerm); err != nil {
+		logger.Fatalf("Error creating %s directory: %v", dataDir, err)
+	}
+	// Create a symbolic link from /data/$serviceName to /var/lib/$serviceName
+	if err := os.Symlink(dataDir, varLibDir); err != nil {
+		logger.Fatalf("Error creating symbolic link: %v", err)
+	}
+	err = os.Chown(dataDir, clickhouseUid, clickhouseGid)
 	logger.CheckErr(err)
 
-	clickhouseUser, err := user.Lookup("clickhouse")
+	if err := os.RemoveAll("/var/lib/clickhouse"); err != nil {
+		logger.Fatalf("Error removing /var/lib/clickhouse: %v", err)
+	}
+
+	finalLogDir := fmt.Sprintf("/logs/%s", serviceName)
+	err = os.MkdirAll(finalLogDir, os.ModePerm)
 	logger.CheckErr(err)
 
 	err = os.MkdirAll("/run/"+serviceName, os.ModePerm)
-	logger.CheckErr(err)
-	clickhouseUid, err := strconv.Atoi(clickhouseUser.Uid)
-	logger.CheckErr(err)
-	clickhouseGid, err := strconv.Atoi(clickhouseUser.Gid)
 	logger.CheckErr(err)
 	err = os.Chown(finalLogDir, clickhouseUid, clickhouseGid)
 	logger.CheckErr(err)
