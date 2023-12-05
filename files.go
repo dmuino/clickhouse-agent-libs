51b8bd6533d34daded2a60f389acd8359c0c5bcc
Daniel Muino <dmuino@netflix.com>
2023-12-04 16:20:10 -0800

    Add file exists/touch functions

--- files.go
+++ files.go
@@ -9,3 +9,19 @@ func CopyFile(inputName string, outputName string) {
 	err = os.WriteFile(outputName, input, 0644)
 	logger.CheckErr(err)
 }
+
+func FileExists(name string) bool {
+	_, err := os.Stat(name)
+	return err == nil
+}
+
+func TouchFile(name string) {
+	if FileExists(name) {
+		return
+	}
+	// ignore errors
+	file, err := os.Create(name)
+	if err != nil {
+		_ = file.Close()
+	}
+}
